997ac733e3624612010bcad06adc897107f84bd4
Youss06 <oumar.youssouf0605@gmail.com>
2017-11-08 11:14:11 +0100

    Add setter and getters

--- entities/Account.php
+++ entities/Account.php
@@ -22,3 +22,57 @@ public function hydrate(array $data)
       }
     }
 }
+
+public function getId()
+    {
+        return $this->id;
+    }
+
+   public function setId($id)
+    {
+        $id = (int)$id;
+          if ($id > 0)
+      {
+        $this->id = $id;
+      }
+    }
+
+   public function getName()
+    {
+        return $this->name;
+    }
+
+   public function setName($name)
+    {
+            if (is_string($name))
+        {
+          $this->name = $name;
+        }
+    }
+
+
+   public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+   public function setFirstName($firstName)
+    {
+          if (is_string($firstName))
+      {
+        $this->firstName = $firstName;
+      }
+    }
+
+   public function getAmount()
+    {
+        return $this->amount;
+    }
+
+   public function setAmount($amount)
+    {
+            if (is_string($amount))
+        {
+          $this->amount = $amount;
+        }
+    }
