4e1aef71d41c19679750c985700f1422504b6e7d
Youss06 <oumar.youssouf0605@gmail.com>
2017-11-08 14:42:14 +0100

    Add all form

--- controllers/index.php
+++ controllers/index.php
@@ -10,13 +10,13 @@ spl_autoload_register("loadClass");
 
 $AccountManager = new accountManager();
 
-if ($_POST) {
-  # code...
-  $account = new account(
-    ['name' => $_POST['name'],
-    'firstName' => $_POST['firstName'],
-    'amount' => $_POST['amount'])
-    $AccountManager->add($account);
-}
+// if ($_POST) {
+//   # code...
+//   $account = new account(
+//     ['name' => $_POST['name'],
+//     'firstName' => $_POST['firstName'],
+//     'amount' => $_POST['amount'])
+//     $AccountManager->add($car);
+// }
 
 include ("../views/indexVue.php");
