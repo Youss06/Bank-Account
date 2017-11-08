0d9cc78b9fbdf6d0fa719a9827ab04e9759c5925
Youss06 <oumar.youssouf0605@gmail.com>
2017-11-08 11:08:28 +0100

    Add constructor and hydrade

--- entities/Account.php
+++ entities/Account.php
@@ -6,3 +6,19 @@ abstract class Account {
   Protected $firstName;
   Protected $amount;
 }
+
+// Constructor
+public function __construct(array $data){
+      $this->hydrate($data);
+    }
+
+    // Hydrate
+public function hydrate(array $data)
+  {
+    foreach ($data as $key => $value) {
+      $method = 'set' . ucfirst($key);
+      if (method_exists($this,$method)) {
+      $this->$method($value);
+      }
+    }
+}
